68daf7f109eda061fbd51e191c01b3588aa88c2d
elenaproulova <elenkaf2233@gmail.com>
2025-01-17 15:54:34 +0500

    add code bot

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import telebot
 
+# Замените 'YOUR_BOT_TOKEN' на токен, который вы получили от BotFather
+TOKEN = '8022656828:AAGoJrXZV52fgqzYmDeAPAIectuwFu538q4'
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик для приёма текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "этот бот лежит на локальном сервере")
+
+# Запуск бота
+bot.polling()
